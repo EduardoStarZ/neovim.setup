1d048c134488a930c666c41019d2719a9e5c1a9d
EduardoStarZ <pathver@gmail.com>
2025-05-21 18:08:25 -0300

    better tab behaviour

--- lua/eduardostarz/set.lua
+++ lua/eduardostarz/set.lua
@@ -1,9 +1,11 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 4 
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
+
+
