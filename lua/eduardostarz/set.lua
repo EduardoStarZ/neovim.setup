f886882a13b3d2e4346602655bb59edc2faf5332
EduardoStarZ <pathver@gmail.com>
2026-02-18 17:38:03 -0300

    actual full transparency for editor

--- lua/eduardostarz/set.lua
+++ lua/eduardostarz/set.lua
@@ -1,10 +1,18 @@
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4 
+vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+vim.api.nvim_set_hl(0, 'LineNr', { bg = "NONE" })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = "NONE" })
+
+vim.api.nvim_set_hl(0, 'DiagnosticSignError', { bg = "NONE" })
+vim.api.nvim_set_hl(0, 'DiagnosticSignWarn', { bg = "NONE" })
+vim.api.nvim_set_hl(0, 'DiagnosticSignInfo', { bg = "NONE" })
+vim.api.nvim_set_hl(0, 'DiagnosticSignHint', { bg = "NONE" })
